0b7c5a7643ef4e67a1d19ed2e6337e9ff87668f8
wzheng1 <wzheng@redhat.com>
2014-04-03 15:49:21 +0800

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,8 @@ RailsApp::Application.configure do
   # Do not compress assets
   config.assets.compress = false
 
+  config.logger = Logger.new(STDOUT)
+  
   # Expands the lines which load the assets
   config.assets.debug = true
 end
