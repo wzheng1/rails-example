13325c894c26a43987c2459892ec82a74ed3bc03
wzheng1 <wzheng@redhat.com>
2014-04-03 17:34:00 +0800

    Update wrap_parameters.rb

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -13,7 +13,4 @@
   +  wrap_parameters ({:format => [:json]})
    end
    
-# Disable root element in JSON by default.
-ActiveSupport.on_load(:active_record) do
-  self.include_root_in_json = false
-end
+
